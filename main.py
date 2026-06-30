f228ea9d7de28918ac482ff8d4098edde6d797f8
GLembrez <gabin.lembrez@ens-paris-saclay.com>
2026-06-30 22:02:25 +0200

    created main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import marimo
+
+__generated_with = "0.23.9"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import numpy as np
+    import casadi as cs
+    from matplotlib import pyplot as plt
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
