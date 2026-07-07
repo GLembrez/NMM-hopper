fixed floquet computation

continuation_functions.py
@@ -38,7 +38,6 @@ def init_trajectory(x0, K, W):
     sol1 = solver(x0, flight, (touch_down, apex), (W,))
     y1 = sol1.y_events[0][0]
     y_a = sol1.y_events[1][0]
-    print(sol1.t_events)
     x02 = np.array([-np.sin(y1[2]), np.cos(y1[2]), y1[3], y1[4]])
     sol2 = solver(x02, stance, (lift_off,), (K,))
     T1 = np.linspace(0, sol1.t_events[0][0], params.N_F)
@@ -57,3 +56,5 @@ def init_trajectory(x0, K, W):
         x2h[:, i] = (1 - alpha2) * sol2.y[:, i2] + alpha2 * sol2.y[:, i2 + 1]
     return x1h, x2h, sol1.t_events[0][0], sol2.t_events[0], y_a, sol1.t_events[1][0]
 
+def energy(x):
+    return 0.5*(x[3]**2 + x[4]**2) + x[1]

main.py
@@ -9,6 +9,7 @@ def _():
     import marimo as mo
     import numpy as np
     import casadi as cs
+    from scipy.linalg import eig
     from matplotlib import pyplot as plt
 
     import floquet 
@@ -18,7 +19,7 @@ def _():
     from continuation_opt import ContinuationSolver
 
 
-    return ContinuationSolver, cs, np, plt, utils
+    return ContinuationSolver, cs, eig, floquet, np, params, plt, utils
 
 
 @app.cell
@@ -33,24 +34,39 @@ def _(ContinuationSolver, cs):
     W = cs.sqrt(10)
 
     solver = ContinuationSolver(K,W)
-    return K, W
+    return K, W, solver
 
 
 @app.cell
-def _(K, W, np, plt, utils):
-    x0 = np.array([0.,1.,0.,0.,1.,0.])
-    xfh,xsh,tf,ts,y_a = utils.init_trajectory(x0,K,W)
-    # print(y_a)
-    plt.plot(xfh[1,:])
-    # plt.plot(xsh[1,:])
-    plt.show()
+def _(K, W, np, params, plt, solver, utils):
+    x0 = np.array([0.,1.,0.,0.,0.1,0.])
+    xfh,xsh,tf,ts,y_a,t_a = utils.init_trajectory(x0,K,W)
+    solver.initialize(ts/params.N_S,tf/params.N_F,xsh,xfh,utils.energy(x0)+0.01)
+    xfh,xsh,dtf,dts = solver.solve()
+    plt.plot(np.linspace(0,params.N_F*dtf,params.N_F),xfh[1,:])
+    plt.plot(np.linspace(params.N_F*dtf,params.N_F*dtf + params.N_S*dts,params.N_S),xsh[1,:])
+    return dtf, dts, xfh, xsh
+
+
+@app.cell
+def _(K, W, cs, dtf, dts, eig, floquet, params, xfh, xsh):
+    var_eval = cs.vertcat(
+        xfh[:,0], xsh[:,0], xfh[:,params.N_F//2], dtf,dts,dtf
+    )
+    x_0 = xfh[:,params.N_F//2]
+    var_eval = floquet.full_newton(var_eval,x_0,K,W)
+    J = floquet.var_newton(var_eval,x_0,K,W)
+    R = floquet.residual_newton(var_eval,x_0,K,W)
+
+    print(eig(J))
+
     return
 
 
 @app.cell
 def _():
     # flight_integrate = dynamics.RK4f.fold(params.N_F // 2)
-    # stance_integrate = dynamics.RK4s.fold(params.N_S)
+    # stance_integrate = dynamics.R.1K4s.fold(params.N_S)
 
     # var = cs.SX.sym("var", 19)
     # x_init = cs.SX.sym("x_init", 6)