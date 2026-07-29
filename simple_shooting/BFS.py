984ab90bb20ecc63470fc6d55450b3b98bcc302b
GLembrez <gabin.lembrez@ens-paris-saclay.com>
2026-07-29 17:44:13 +0200

    working one branch cpntinuation

--- simple_shooting/BFS.py
+++ simple_shooting/BFS.py
@@ -1,8 +1,10 @@
 import casadi as cs
 import numpy as np
+from collections import deque
+from matplotlib import pyplot as plt
 from scipy.linalg import null_space
 from time import time 
-from matplotlib import pyplot as plt
+from copy import deepcopy
 
 np.set_printoptions(precision=9)
 
@@ -88,84 +90,73 @@ phi = cs.SX.sym("phi",10)
 b = du1.T @ cs.jtimes(R_tilde, u, du2).T @ phi
 hessian = cs.Function('Hessian',[u,du1,du2,phi],[b])
 
+n_branch = 3
 z0 = np.array([0.0,1.01,0.,0.,0.,0.,0.1,0.01,0.1,0.0])
-E = z0[1]
-d = 0.005
-p_list = []
-u_list = []
-precision = 0
+E0 = z0[1]
+z_star = newton_step(z0,E0)
+R_eval = compute_R(cs.vertcat(z_star,z_star[1]))
+p = null_space(R_eval)
+to_explore = deque()
+idx_branch=0
+connected_component = []
+to_explore.appendleft((cs.vertcat(z_star,E0),p,1))
 
-start = time()
-for i in range(1000):
-    z_star = newton_step(z0,E)
-    u_list.append(cs.vertcat(z_star,E))
-    R_eval = compute_R(cs.vertcat(z_star,z_star[1]))
-    p = null_space(R_eval)
-    if i>5 and p_list[i-2].T @ p_list[i-1] < 0:
-        d = 0.5 * d
-        precision += 1
-    if np.linalg.det(cs.vertcat(R_eval,p.T)) < 0:
-        p = -p
-    # else:
-    u_next = cs.vertcat(z_star,E) +  d * p
-    if precision == 10:
-        print(i)
-        break
-    z0 = u_next[:10]
-    E = u_next[10]
-    p_list.append(p)
-end = time()
-print(end-start,E)
+while to_explore and idx_branch<n_branch:
+    branch = []
+    p_list = []
+    u_next,p,sign = to_explore.popleft()
+    idx_branch += 1
+    precision = 0
+    d = np.sign(sign) * 0.005
 
-c1 = -p 
-val,vec = np.linalg.eig(cs.vertcat(R_eval,c1.T))
-idx_val = np.argmin(np.abs(val))
-c2 = vec[:,idx_val].real
-val,vec = np.linalg.eig(R_eval@R_eval.T)
-idx_val = np.argmin(np.abs(val))
-e = vec[:,idx_val].real
+    for i in range(1000):
+        if np.linalg.det(cs.vertcat(R_eval,p.T)) < 0:
+            p = -p
 
-b11 = hessian(u_next,c1,c1,e)
-b12 = hessian(u_next,c1,c2,e)
-b22 = hessian(u_next,c2,c2,e)
+        branch.append(cs.vertcat(z_star,E0))
+        p_list.append(p)
+        u_next = cs.vertcat(z_star,E0) +  d * p
+        z0 = u_next[:10]
+        E0 = u_next[10]
+        z_star = newton_step(z0,E0)
+        R_eval = compute_R(cs.vertcat(z_star,z_star[1]))
+        p = null_space(R_eval)
 
-beta2 = 1
-beta1 = -b22/(2*b12)
-c_star = beta1*c1 + beta2*c2
-c_star = c_star / np.linalg.norm(c_star)
 
-d = 0.01
-precision = 0
-u_next = cs.vertcat(z_star,E) +  d * c_star
-z0 = u_next[:10]
-E = u_next[10]
-start = time()
-for i in range(1000):
-    print(z0[3])
-    z_star = newton_step(z0,E)
-    u_list.append(cs.vertcat(z_star,E))
-    R_eval = compute_R(cs.vertcat(z_star,z_star[1]))
-    p = null_space(R_eval)
-    # if i>5 and p_list[i-2].T @ p_list[i-1] < 0:
-    #     d = 0.5 * d
-    #     precision += 1
-    if np.linalg.det(cs.vertcat(R_eval,p.T)) < 0:
-        d = -np.abs(d)
-    else:
-        d = np.abs(d)
-    # else:
-    u_next = cs.vertcat(z_star,E) +  d * p
-    # if precision == 10:
-        # break
-    z0 = u_next[:10]
-    E = u_next[10]
-    p_list.append(p)
-end = time()
-print(F(u_next))
+        if precision == 10:
+            connected_component.append(branch)
+            c1 = -deepcopy(p)
+            val,vec = np.linalg.eig(cs.vertcat(R_eval,c1.T))
+            idx_val = np.argmin(np.abs(val))
+            c2 = vec[:,idx_val].real
+            val,vec = np.linalg.eig(R_eval@R_eval.T)
+            idx_val = np.argmin(np.abs(val))
+            e = vec[:,idx_val].real
+
+            b11 = hessian(u_next,c1,c1,e)
+            b12 = hessian(u_next,c1,c2,e)
+            b22 = hessian(u_next,c2,c2,e)
+
+            beta2 = 1
+            beta1 = -b22/(2*b12)
+            c_star = beta1*c1 + beta2*c2
+            c_star = c_star / np.linalg.norm(c_star)
+
+            to_explore.append((cs.vertcat(deepcopy(z_star),deepcopy(E0)),deepcopy(c1),-np.sign(deepcopy(sign))))
+            to_explore.append((cs.vertcat(deepcopy(z_star),deepcopy(E0)),deepcopy(c_star),-np.sign(deepcopy(sign))))
+            break
+        
+
+        if i>5 and p_list[i-2].T @ p_list[i-1] < 0:
+            d = 0.5 * d
+            precision += 1
+
+        
 
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1,projection="3d")
-branch = np.array(u_list)
-plt.plot(branch[:,1],branch[:,2],branch[:,3])
+for i in range(len(connected_component)):
+    branch = np.array(connected_component[i])
+    plt.plot(branch[:,1],branch[:,2],branch[:,3])
 plt.show()
