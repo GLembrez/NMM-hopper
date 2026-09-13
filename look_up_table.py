59826ca327e82c7c16bc472d070037b4da37f30a
GLembrez <gabin.lembrez@ens-paris-saclay.com>
2026-09-13 15:05:00 +0200

    added second step

--- look_up_table.py
+++ look_up_table.py
@@ -50,8 +50,8 @@ def build_solver(data,N_SAMPLES,R,reverse=False):
     MPC_solver = MPCSolver(40,1,50,R,LUT_list)
     return MPC_solver
 
-data_locomotion = pickle.load(open('data/locomotion.pkl', 'rb'))
-data_switch = pickle.load(open('data/switch.pkl','rb'))
+data_locomotion = pickle.load(open('data/locomotion4.pkl', 'rb'))
+data_switch = pickle.load(open('data/switch4.pkl','rb'))
 NMM_locomotion = np.array(data_locomotion[2])
 NMM_switch = np.array(data_switch[2])
 
@@ -64,36 +64,52 @@ u_star = NMM_locomotion[100]
 u_switch = NMM_switch[np.argmin(np.abs(NMM_switch[:,-1]-u_star[-1]))]
 traj_star = get_trajectory(NMM_solver,u_star)
 traj_switch = get_trajectory(NMM_solver,u_switch)
-# traj_switch[[0,2,3,5],:] = - traj_switch[[0,2,3,5],:]
-x0 = traj_star[[0,1,3,4],0]
-xs = traj_star[[0,1,3,4],:50]
-xf = traj_star[:,50:]
-# xf = traj_switch[:,50:].copy()
-# xf[[0,2,3,5],:] = - xf[[0,2,3,5],:]
-# xf[0,:] += -xf[0,0] + traj_star[0,50]
-dt = cs.vertcat(u_star[7],u_star[6]+u_star[8])
-solver_RL.initialize(x0,xs,xf,dt)
-xs_star,xf_star,dt_star,cmd = solver_RL.solve()
-traj_next = NMM_solver.traj_s_list(NMM_solver.flight_to_stance(xf_star[:,-1]),dt_star[0],0)
-traj_next[0,:] += xf_star[0,-1] - traj_next[0,0]
-# x_TD_star = xf_star[[0,1,3,4],-1]
-# x_TD_star[0] = -cs.sin(xf_star[2,-1])
-# e = NMM_solver.energy_flight(xf_star[:,-1])
-# print(x_TD_star)
-# print(cs.vertcat(solver_RL.LUT_x(e),solver_RL.LUT_y(e),solver_RL.LUT_dx(e),solver_RL.LUT_dy(e)))
-# print(cs.vertcat(solver_R.LUT_x(e),solver_R.LUT_y(e),solver_R.LUT_dx(e),solver_R.LUT_dy(e)))
 
+
+x01 = traj_star[[0,1,3,4],0]
+xs1 = traj_star[[0,1,3,4],:50]
+xf1 = traj_star[:,50:]
+dt1 = cs.vertcat(u_star[7],u_star[6]+u_star[8])
+solver_RL.initialize(x01,xs1,xf1,dt1)
+xs_star,xf_star,dt_star,cmd = solver_RL.solve()
+
+x02 = xf_star[[0,1,3,4],-1]
+x02[0] = -cs.sin(xf_star[2,-1])
+xs2 = traj_switch[[0,1,3,4],:50]
+xf2 = traj_star[:,50:]
+xf2[[0,2,3,5],:] = -xf2[[0,2,3,5],:] 
+xf2[0,:] += xs2[0,-1] - xf2[0,0]
+dt2 = cs.vertcat(u_switch[7],u_switch[6]+u_switch[8])
+# x0[0] += -cs.sin(xf_star[2,-1])-x0[0]
+# xs[0,:] += -cs.sin(xf_star[2,-1])-xs[0,0]
+# xf[0,:] += -cs.sin(xf_star[2,-1])-xf[0,0]
+
+
+solver_L.initialize(x02,xs2,xf2,dt2)
+e = NMM_solver.energy_flight(xf_star[:,-1])
+print(solver_L.LUT_x(e),solver_L.LUT_y(e),solver_L.LUT_dx(e),solver_L.LUT_dy(e))
+print(xf2[:,-1])
+print(x02)
+print(xs2[:,0])
+xs_star1,xf_star1,dt_star1,cmd1 = solver_L.solve()
+
+traj_next = NMM_solver.traj_s_list(NMM_solver.flight_to_stance(xf_star1[:,-1]),dt_star1[0],0)
+traj_next[0,:] += xf_star[0,-1] - traj_next[0,0]
 
 fig = plt.figure()
 # plt.plot(NMM[:,1],NMM[:,3])
 # plt.plot(NMM_sampled[:,1],NMM_sampled[:,3])
 # plt.plot(LUT_y(E_linspace),LUT_dx(E_linspace))
-plt.plot(xs[0,:],xs[1,:],'blue')
-plt.plot(xf[0,:],xf[1,:],'blue')
+# plt.plot(xs1[0,:],xs1[1,:],'teal')
+# plt.plot(xf1[0,:],xf1[1,:],'teal')
+# plt.plot(xs2[0,:],xs2[1,:],'blue')
+# plt.plot(xf2[0,:],xf2[1,:],'blue')
 plt.plot(xs_star[0,:],xs_star[1,:],'red')
 plt.plot(xf_star[0,:],xf_star[1,:],'red')
-plt.plot(traj_star[0,:],traj_star[1,:])
-plt.plot(traj_switch[0,:],traj_switch[1,:])
+plt.plot(xs_star1[0,:],xs_star1[1,:],'green')
+plt.plot(xf_star1[0,:],xf_star1[1,:],'green')
+# plt.plot(traj_star[0,:],traj_star[1,:])
+# plt.plot(traj_switch[0,:],traj_switch[1,:])
 plt.plot(traj_next[0,:].T,traj_next[1,:].T)
 # plt.plot(cmd.T)
 plt.show()
